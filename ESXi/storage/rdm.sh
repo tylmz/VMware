ad247f8ad9dd4636bcbd2dfaa606f1a4a4aee616
Tolgahan YILMAZ <tolgahan_y@yahoo.com.tr>
2020-02-07 18:11:57 +0300

    Update rdm.sh

--- ESXi/storage/rdm.sh
+++ ESXi/storage/rdm.sh
@@ -8,13 +8,13 @@ echo "Currently $currentNum disks set as Perennially Reserved"
 NUM=1
 dNUM=`cat disk.list | wc -l`
 while [ $NUM -le $dNUM ]
-do
-naa=$(sed -n "$NUM,1p" disk.list)
-esxcli storage core device setconfig -d "$naa" --perennially-reserved=true
-echo "Iteration number: $NUM"
-echo "$naa set as perennially-reserved going for next one"
-NUM=`expr $NUM + 1`
-done
+ do
+  naa=$(sed -n "$NUM,1p" disk.list)
+  esxcli storage core device setconfig -d "$naa" --perennially-reserved=true
+  echo "Iteration number: $NUM"
+  echo "$naa set as perennially-reserved going for next one"
+  NUM=`expr $NUM + 1`
+ done
  
 lastNum=$(/bin/esxcli storage core device list | egrep -Ei "Reserved: true" | wc -l)
 echo "Currently $lastNum disks set as Perennially Reserved";
