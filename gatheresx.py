013e795fd77a7fe95ec023729ec79836f9ef3630
Tolgahan YILMAZ <tolgahan_y@yahoo.com.tr>
2020-03-12 20:17:08 +0300

    Update gatheresx.py

--- gatheresx.py
+++ gatheresx.py
@@ -3,6 +3,7 @@ import argparse
 import time
 import socket
 import sys
+from os import path
 
 parser = argparse.ArgumentParser(description='Lets Get Started, tyilmaz@vmware.com for questions and help')
 parser.add_argument("--hostname",action='store_true', help='gather hostnames')
@@ -10,6 +11,9 @@ parser.add_argument("--niclist",action='store_true', help='gather NIC info')
 parser.add_argument("--adapters",action='store_true', help='gather HBA info')
 parser.add_argument("--device",action='store_true', help='gather storage device info')
 parser.add_argument('--cmd', action='store_true', help='Use this to execute custom command')
+parser.add_argument("--file", action='store_true', help='File that contains hostlist')
+parser.add_argument("--out", action='store_true', help='Logfile for output')
+parser.add_argument("--vmnetstat", action='store_true', help='Gather vNIC stats')
 args = parser.parse_args()
 cmdlet = []
 hostlist = []
@@ -70,7 +74,32 @@ def rem_host ():
             else:
                 return True
 
-            
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+if args.file:
+    print('Please enter a command to execute on ESXi Shell')
+    filepath = str(input('File Path That Contains Hostlist :'))
+    correction = False
+    while correction == False:
+        print('Please check if you have entered File Path That Contains Hostlist')
+        correction = input('Is this correct path? : '+filepath+' (Y/N):')
+        if correction == "Y":
+            file_path = path.relpath(filepath)
+            with open(file_path) as input_file:
+                hostlist = [line.strip() for line in input_file]
+            input_file.close()
+            correction = True
+        else:
+            sys.exit('Bye Bye')
+else:
+    hostquery = ['shell \n chsh -s /bin/bash \n /opt/vmware/vpostgres/current/bin/psql -U postgres VCDB -c "select ip_address from vpx_host;" | egrep -Evi "rows|ip|--" > /tmp/hostlist'
+             ]
+    vc = Infra_connect(str(input('vCenter Address :')), str(input('Username :')),str(input('Password :')))
+    hostlist = Infra_connect.get_hosts()
+    rem_host=rem_host()
+
+    
 if args.hostname:
                 cmdlet = {'touch "/vmfs/volumes/NFS10/`hostname`.txt"'}
 elif args.niclist:
@@ -87,15 +116,20 @@ elif args.cmd:
                     print('Please check if you have entered correct command')
                     correction = input('Are you sure to execute : '+inputcmd+' (Y/N):')
                     if correction == "Y":
-                        inputcmd = inputcmd+' >> "/vmfs/volumes/NFS/custom.txt"'
+                        inputcmd = inputcmd+' >> "/vmfs/volumes/iSCSI/custom.txt"'
                         correction = True
                     else:
                         sys.exit('Bye Bye')
-                cmdlet = ['echo "`hostname`" >> "/vmfs/volumes/NFS/custom.txt" ',
-                          'echo "\n" >> "/vmfs/volumes/NFS/custom.txt" ',
+                cmdlet = ['echo "`hostname`" >> "/vmfs/volumes/iSCSI/custom.txt" ',
+                          'echo "\n" >> "/vmfs/volumes/iSCSI/custom.txt" ',
                           inputcmd,
-                          'echo "\n" >> "/vmfs/volumes/NFS/custom.txt" ',
+                          'echo "\n" >> "/vmfs/volumes/iSCSI/custom.txt" ',
                           ]
+elif args.vmnetstat:
+                cmdlet = ["echo '`hostname`' >> '/vmfs/volumes/iSCSI/vmnetstat.txt' ",
+                          "echo '\n' >> '/vmfs/volumes/iSCSI/vmnetstat.txt'",
+                          "for i in `vsish -e ls /net/portsets/ | cut -c 1- | sed 's:/.*::'`; do for p in `vsish -e ls /net/portsets/$i/ports | cut -c 1- | sed 's:/.*::'`; do vsish -e cat /net/portsets/$i/ports/$p/status | grep 'clientName' >> /vmfs/volumes/iSCSI/vmnetstat.txt; vsish -e cat /net/portsets/$i/ports/$p/stats | egrep -E 'droppedTx|droppedRx' >> /vmfs/volumes/iSCSI/vmnetstat.txt; vsish -e cat /net/portsets/$i/ports/$p/vmxnet3/rxSummary 2>/dev/null  | egrep -E 'running out of buffers|1st ring size|of times the 1st ring is full|2nd ring size|2nd ring is full' >> /vmfs/volumes/iSCSI/vmnetstat.txt; echo '---------------------------' >> /vmfs/volumes/iSCSI/vmnetstat.txt; done done;",
+                          "echo '\n' >> '/vmfs/volumes/iSCSI/vmnetstat.txt'"]
 else:
             cmdlet = ['echo "`hostname`" >> "/vmfs/volumes/NFS10/environment.txt" ',
                       'echo "\n" >> "/vmfs/volumes/NFS10/environment.txt"',
@@ -113,16 +147,10 @@ else:
                       'echo "\n" >> "/vmfs/volumes/NFS10/environment.txt"'
                       ]
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-hostquery = ['shell \n chsh -s /bin/bash \n /opt/vmware/vpostgres/current/bin/psql -U postgres VCDB -c "select ip_address from vpx_host;" | egrep -Evi "rows|ip|--" > /tmp/hostlist'
-             ]
-
-vc = Infra_connect(str(input('vCenter Address :')), str(input('Username :')),str(input('Password :')))
 #Infra_connect.get_hosts()
-hostlist = Infra_connect.get_hosts()
-rem_host=rem_host()
+
+
 if rem_host == True:
     sys.exit("Please open all SSH ports")
 print(hostlist)
